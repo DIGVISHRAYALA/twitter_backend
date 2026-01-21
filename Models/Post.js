828cf89178b67cdf0ca0cd360eca96f3c034d420
DIGVISHRAYALA <152776370+DIGVISHRAYALA@users.noreply.github.com>
2026-01-21 13:22:22 +0530

    Add files via upload

---
+++ Models/Post.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+
+const PostSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true
+    },
+    postContent: {
+        type: String,
+        required: true
+    },
+    profilepic: {
+        type: String,
+        required: true
+    },
+    postImage: {
+        type: String,
+    },
+    totalLikes: {
+        type: Number,
+        default: 0
+    },
+    totalTweets: {
+        type: Number,
+        default: 0
+    },
+    createdAtDate: {
+        type:Date,
+        default: Date.now,
+    }
+})
+
+const Post = mongoose.model('Post', PostSchema)
+module.exports = Post

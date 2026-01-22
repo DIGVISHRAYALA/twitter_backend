b23e2dbe8230d7a1052560a6cb5093e4be9ea698
DIGVISHRAYALA <152776370+DIGVISHRAYALA@users.noreply.github.com>
2026-01-22 14:17:37 +0530

    Migrate Follow model from Mongoose to Sequelize

--- Models/FollowingFollower.js
+++ Models/FollowingFollower.js
@@ -1,15 +1,46 @@
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
 
-const FollowSchema = new mongoose.Schema({
-    username: {
-        type: String,
-        required: true
+// const FollowSchema = new mongoose.Schema({
+//     username: {
+//         type: String,
+//         required: true
+//     },
+//     following: {
+//         type: String,
+//         required: true
+//     }
+// })
+
+// const Follow = mongoose.model('Follow', FollowSchema)
+
+// module.exports = Follow;
+
+
+
+
+
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/db');
+
+const Follow = sequelize.define('Follow', {
+    id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
     },
-    following: {
-        type: String,
-        required: true
-    }
-})
 
-const Follow = mongoose.model('Follow', FollowSchema)
+    username: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+
+    following: {
+        type: DataTypes.STRING,
+        allowNull: false
+    }
+}, {
+    tableName: 'follows',
+    timestamps: true
+});
+
 module.exports = Follow;
